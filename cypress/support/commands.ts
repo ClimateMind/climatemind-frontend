d1182b95c0bb9126b9cc845128b49ed6650ee1d2
Matt Charlton <Matt.charlton@noredink.com>
2020-08-03 22:35:16 +0100

    add example custom command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,3 +24,9 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+
+
+Cypress.Commands.add('testCommand', () => {
+  // TODO: Delete this once we have real commands that can be used as examples
+  cy.log('Hello')
+})
