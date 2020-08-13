0084b3d3b9ac8c850ba7d9eace8956434b05ac5a
thiyagarajan-angappan <thiyagarajan.angappan@gmail.com>
2020-08-13 16:48:43 -0500

    remove unused box

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box } from '@material-ui/core';
 
 const styles = makeStyles({
     root: {
