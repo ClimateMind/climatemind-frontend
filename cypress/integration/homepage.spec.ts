e69049c2857a50bc4119b390355f2296120269fc
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-08-22 16:17:00 +0100

    add accessibility check to homepage test

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -4,6 +4,7 @@ describe('Homepage loads and looks correct', () => {
   it('can open homepage', () => {
     cy.visit('');
     cy.contains('Get started').should('be.visible');
+    cy.checkAccessibility()
     cy.percySnapshot('Homepage');
   });
 });
