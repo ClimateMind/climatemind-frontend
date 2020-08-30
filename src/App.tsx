b6e7eb65cc0081b4d030e0d18ab4253e37622a68
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-30 12:03:09 +0100

    Adding welcome page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import './common/styles/global.scss';
 import Home from './pages/Home';
+import Welcome from './pages/Welcome';
 
 const CMTheme = createMuiTheme({
   typography: {
-    fontFamily: 'atten-round-new, sans-serif'
-  }
+    fontFamily: 'atten-round-new, sans-serif',
+  },
 });
 
 const App = () => {
   return (
     <MuiThemeProvider theme={CMTheme}>
-       <Home />
+      {/* <Home /> */}
+      <Welcome />
     </MuiThemeProvider>
   );
-}
+};
 
 export default App;
