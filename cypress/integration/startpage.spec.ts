5db7f8215bc698660d4895ddc31264b1b25934e3
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-09-03 18:38:05 +0100

    add start page test

---
+++ cypress/integration/startpage.spec.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+import {terminalLog} from '../support/helpers.ts'
+
+describe('Homepage loads and looks correct', () => {
+  beforeEach(() => {
+    cy.visit('/start');
+  })
+
+  it('can open homepage', () => {
+    cy.contains('Hello there!').should('be.visible');
+    cy.checkAccessibility(terminalLog)
+    cy.percySnapshot('Start Page')
+  });
+
+  it('Lets go button goes to quiz', () => {
+    cy.contains('Let\'s Go').should('be.visible').click()
+    cy.url().should('include', '/quiz')
+  })
+});
