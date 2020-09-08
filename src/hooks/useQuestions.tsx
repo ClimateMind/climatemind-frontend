922e8d03047c47788529c7c212fd9cb26d0b923e
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-09-08 17:59:00 +0100

    remove ranomisation for now

--- src/hooks/useQuestions.tsx
+++ src/hooks/useQuestions.tsx
@@ -1,22 +1,8 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { QuestionsContext } from '../contexts/questions';
 
 export const useQuestions = () => {
   const questions = useContext(QuestionsContext);
 
-  // The questions from the api should be shuffled into a random order
-  // Currently shuffling the items based on sorting the questions arrays based on to random numbers values. not sure if it us truely random but seems to be random enough.
-  const shuffledQuestions = { ...questions };
-
-  if (questions.SetOne && questions.SetTwo) {
-    const shuffledSetOne = questions.SetOne.sort(
-      () => Math.random() - Math.random()
-    );
-    const shuffledSetTwo = questions.SetTwo.sort(
-      () => Math.random() - Math.random()
-    );
-    shuffledQuestions.SetOne = shuffledSetOne;
-    shuffledQuestions.SetTwo = shuffledSetTwo;
-  }
-  return shuffledQuestions;
+  return questions;
 };
