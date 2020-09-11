e54a86acf136f502333dc337f0072906c9b282ee
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-09-11 17:22:50 +0100

    Adding missing dep

--- src/contexts/questions.tsx
+++ src/contexts/questions.tsx
@@ -1,32 +1,35 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
-import { Questions } from "../types/types";
+import { Questions } from '../types/types';
 
 export const QuestionsContext = createContext<Questions>({} as Questions);
 
 export const QuestionsProvider: React.FC = ({ children }) => {
-    const [questions, setQuestions] = useState({} as Questions);
-    const API_HOST = (process.env.NODE_ENV === "development") ? "http://localhost:5000" : process.env.REACT_APP_API_URL;
-    const QUESTIONS_ENDPOINT = "/questions";
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          console.log("API_URL: ", API_HOST + QUESTIONS_ENDPOINT);
-          const request = await axios.get(API_HOST + QUESTIONS_ENDPOINT);
-          const data = request.data;
-          setQuestions(data);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      fetchData();
-    }, []);
-  
-    return (
-      <QuestionsContext.Provider value={questions}>
-        {children}
-      </QuestionsContext.Provider>
-    );
-  };
+  const [questions, setQuestions] = useState({} as Questions);
+  const API_HOST =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : process.env.REACT_APP_API_URL;
+  const QUESTIONS_ENDPOINT = '/questions';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('API_URL: ', API_HOST + QUESTIONS_ENDPOINT);
+        const request = await axios.get(API_HOST + QUESTIONS_ENDPOINT);
+        const data = request.data;
+        setQuestions(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [API_HOST]);
+
+  return (
+    <QuestionsContext.Provider value={questions}>
+      {children}
+    </QuestionsContext.Provider>
+  );
+};
