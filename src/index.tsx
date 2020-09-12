c398ad8e77bb288f044be4fad69e37f61f2e44b9
thiyagarajan-angappan <thiyagarajan.angappan@gmail.com>
2020-09-12 14:18:47 -0500

    add csp for styles from typekit

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { QuestionsProvider } from './contexts/questions';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta 
+        httpEquiv="Content-Security-Policy"
+        content="style-src 'self' https://use.typekit.net/"
+      />
+    </Helmet>
     <QuestionsProvider>
       <App />
     </QuestionsProvider>
