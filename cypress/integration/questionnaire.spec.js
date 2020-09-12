699afe740053efdcae32ab04b60d6c8d3068cafe
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-09-12 16:55:12 +0100

    complete the questionnaire

--- cypress/integration/questionnaire.spec.js
+++ cypress/integration/questionnaire.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import {terminalLog} from '../support/helpers.ts'
+import { terminalLog } from '../support/helpers.ts';
 
 describe('Questionnaire loads and looks correct', () => {
   before(() => {
@@ -8,9 +8,9 @@ describe('Questionnaire loads and looks correct', () => {
     cy.route({
       method: 'GET',
       url: '/questions',
-      response: 'fixture:questions.json'
+      response: 'fixture:questions.json',
     });
-  })
+  });
 
   beforeEach(() => {
     cy.visit('/questionnaire');
@@ -36,5 +36,20 @@ describe('Questionnaire loads and looks correct', () => {
   it('Can answer questions in the questionnaire', () => {
     cy.contains('Like Me').should('be.visible').click();
     cy.contains('Q2.').should('be.visible');
-  })
+  });
+
+  it('Can complete the questionnaire with random answers', () => {
+    cy.fixture('questions').then((questions) => {
+      let question = 1;
+      while (question <= 10) {
+        const randomAnswer = Math.floor(Math.random() * 6) + 1;
+        const nextQuestion =
+          question < 10 ? `Q${question + 1}` : 'Woohoo! Good Job!';
+        cy.contains(questions.Answers[randomAnswer]).click();
+        cy.contains(nextQuestion).should('be.visible');
+        cy.log(question);
+        question++;
+      }
+    });
+  });
 });
