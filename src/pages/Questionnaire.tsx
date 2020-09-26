9d1e2390297a9d3908cb68bf5686e1386529d037
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-09-26 17:59:36 +0100

    remove uneeded import

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Question from '../components/Question';
 import { useQuestions } from '../hooks/useQuestions';
 import { TQuestion } from '../types/types';
