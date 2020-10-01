f5a9e1904c8b3220bda26be99bd1b8ff4ac697f2
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-10-01 19:05:35 +0100

    add cypress test for personality page

---
+++ cypress/integration/personalitypage.spec.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+import {terminalLog} from '../support/helpers.ts'
+
+describe('Personality page loads and looks correct', () => {
+  beforeEach(() => {
+    cy.visit('/personality');
+  })
+
+  it('can open personality page', () => {
+    cy.contains('Good to meet you').should('be.visible');
+    //TODO - once name is dynamic we should check for that
+    cy.checkAccessibility(terminalLog)
+    cy.percySnapshot('Personality Page - Collapsed')
+  });
+
+  it('can use personality dropdown', () => {
+    cy.contains('To make decisions we each employ three personal values.')
+      .should('not.be.visible')
+    cy.get('[id="icon-arrow-down"]').click()
+    cy.contains('To make decisions we each employ three personal values.')
+      .should('be.visible')
+    cy.percySnapshot('Personality Page - Collapsed')
+    cy.get('[id="icon-arrow-down"]').click()
+    cy.contains('To make decisions we each employ three personal values.')
+      .should('not.be.visible')
+  })
+
+  it('Lets go button goes to climate personality page', () => {
+    cy.contains('Take the quiz').should('be.visible').click()
+    cy.url().should('include', '/questionnaire')
+  });
+});
