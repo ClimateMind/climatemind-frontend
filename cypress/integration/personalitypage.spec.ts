0e73dba7ed4198c8e0a176d2d3e4f6aa946f0d94
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-10-01 19:16:54 +0100

    rename second snapshot

--- cypress/integration/personalitypage.spec.ts
+++ cypress/integration/personalitypage.spec.ts
@@ -20,7 +20,7 @@ describe('Personality page loads and looks correct', () => {
     cy.get('[id="icon-arrow-down"]').click()
     cy.contains('To make decisions we each employ three personal values.')
       .should('be.visible')
-    cy.percySnapshot('Personality Page - Collapsed')
+    cy.percySnapshot('Personality Page - Expanded')
     cy.get('[id="icon-arrow-down"]').click()
     cy.contains('To make decisions we each employ three personal values.')
       .should('not.be.visible')
