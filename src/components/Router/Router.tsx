c251e0b00df735a315b887ca92520f414c1ba568
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-11-08 10:27:51 +0000

    Add PageWithAppBar

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../../pages/Home';
 import MeetGuy from '../../pages/MeetGuy';
-import CMAppBar from '../AppBar/AppBar';
 import ClimatePersonality from '../../pages/ClimatePersonality';
 import Quiz from '../../pages/Questionnaire';
 import Error404 from '../../pages/Error404';
@@ -10,37 +9,48 @@ import SubmitQuestionnaire from '../../pages/SubmitQuestionnaire';
 import PersonalValues from '../../pages/PersonalValuesFeed';
 import ClimateFeed from '../../pages/ClimateFeed';
 import ROUTES from '../Router/RouteConfig';
+import PageWithAppBar from '../AppBar/PageWithAppBar';
 
 const Router = () => {
   return (
     <BrowserRouter>
       {/* Switch ensures app bar not displayed on landing screen */}
 
-      <Route path={ROUTES.ROUTE_HOME}>
-        <CMAppBar />
-      </Route>
+      {/* <Route path={ROUTES.ROUTE_HOME}><CMAppBar /></Route> */}
 
       <Switch>
         <Route exact path={ROUTES.ROUTE_HOME} render={() => <Home />} />
-        <Route exact path={ROUTES.ROUTE_QUIZHOME} render={() => <MeetGuy />} />
+        <Route
+          exact
+          path={ROUTES.ROUTE_QUIZHOME}
+          render={() => <PageWithAppBar component={<MeetGuy />} />}
+        />
         <Route exact path={ROUTES.ROUTE_QUIZHOME} render={() => <MeetGuy />} />
         <Route
           exact
           path={ROUTES.ROUTE_PERSONALITY}
-          render={() => <ClimatePersonality />}
+          render={() => <PageWithAppBar component={<ClimatePersonality />} />}
         />
-        <Route exact path={ROUTES.ROUTE_QUIZ} render={() => <Quiz />} />
+        <Route
+          exact
+          path={ROUTES.ROUTE_QUIZ}
+          render={() => <PageWithAppBar component={<Quiz />} />}
+        />
         <Route
           exact
           path={ROUTES.ROUTE_SUBMIT}
-          render={() => <SubmitQuestionnaire />}
+          render={() => <PageWithAppBar component={<SubmitQuestionnaire />} />}
         />
         <Route
           exact
           path={ROUTES.ROUTE_VALUES}
-          render={() => <PersonalValues />}
+          render={() => <PageWithAppBar component={<PersonalValues />} />}
         />
-        <Route exact path={ROUTES.ROUTE_FEED} render={() => <ClimateFeed />} />
+        <Route
+          exact
+          path={ROUTES.ROUTE_FEED}
+          render={() => <PageWithAppBar component={<ClimateFeed />} />}
+        />
         <Route path="*" render={() => <Error404 />} />
       </Switch>
     </BrowserRouter>
