00ea06af4eb5c9be27c171e546a8c015fca19d36
Matt Charlton <mattcharlton@Matts-MacBook.local>
2020-11-08 18:06:53 +0000

    percy captures for error pages

---
+++ cypress/integration/errorpages.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import { terminalLog } from '../support/helpers.ts';
+
+describe('Error pages load and look correct', () => {
+  it('can visit 404 error page', () => {
+    cy.visit('/fakepage');
+    cy.contains('Well this is awkward').should('be.visible');
+    cy.checkAccessibility(terminalLog);
+    cy.percySnapshot('404 Error Page');
+
+    cy.contains('a', 'Homepage').should('have.attr', 'href', '/');
+    cy.contains('Go to homepage').click();
+    cy.url().should('equal', Cypress.config().baseUrl);
+  });
+
+  it('can visit 500 error page', () => {
+    cy.visit('/questionnaire');
+    cy.contains('It’s broken…').should('be.visible');
+    cy.checkAccessibility(terminalLog);
+    cy.percySnapshot('500 Error Page');
+  });
+});
