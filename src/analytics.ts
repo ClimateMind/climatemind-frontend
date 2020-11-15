22d60cfdf1cc2d3346c065041504ce69b4e9f384
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-11-15 11:04:23 +0000

    add return

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,7 +1,7 @@
 import TagManager from 'react-gtm-module';
 
 // This has been added to the prettier ignore file as tag manager does not pick up events when trailing commas are added to the data layer object
-export const pushQuestionToDataLayer = (questionId: number) => {
+export const pushQuestionToDataLayer = (questionId: number): void  => {
   TagManager.dataLayer({
     dataLayer: {
       event: 'event',
