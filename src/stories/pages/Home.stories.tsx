d0633a5075b2aa0bc61ed81c4ec10621edc67a21
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-11-24 11:26:38 +0000

    rm mui theme from page

--- src/stories/pages/Home.stories.tsx
+++ src/stories/pages/Home.stories.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { MuiThemeProvider } from '@material-ui/core';
-import CMTheme from '../../common/styles/CMTheme';
 import Home from '../../pages/Home';
 
 export default {
   title: 'ClimateMind/pages/HomePage',
-  component: Home,
-  decorators:  [
-    (Story) => (
-      <MuiThemeProvider theme={CMTheme}>
-        <Story />
-      </MuiThemeProvider>
-    ),]
+  component: Home
 } as Meta;
 
 const Template: Story<{}> = (args) => <Home {...args} />;
