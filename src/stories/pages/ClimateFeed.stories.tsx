4c3cc9beb77a6c970c8f575f78ec98608d593b0d
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-11-25 21:25:50 +0000

    prettier

--- src/stories/pages/ClimateFeed.stories.tsx
+++ src/stories/pages/ClimateFeed.stories.tsx
@@ -5,86 +5,89 @@ import { Grid, makeStyles, Toolbar } from '@material-ui/core';
 import Loader from '../../components/Loader';
 import CMCard from '../../components/CMCard';
 import CMCardOverlay from '../../components/CMCardOverlay';
+import AppBar from '../../components/AppBar/AppBar';
 
 // import ClimateFeed from '../../pages/ClimateFeed';
 
-
 // create a test component, decoupled from context...
 const styles = makeStyles({
-    root: {
-      flexGrow: 1,
-      backgroundColor: '#70D7CC',
-      minHeight: '100vh',
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#70D7CC',
+    minHeight: '100vh',
+  },
+  typography: {
+    textAlign: 'center',
+  },
+});
+
+const ClimateFeed: React.FC = () => {
+  const classes = styles();
+  // const climateFeed = useClimateFeed();
+  const climateFeed = [
+    {
+      effectDescription: 'No short desc available at present',
+      effectId: 'R8t0oNsG3WgnupXsBVSjMHZ',
+      effectScore: 14,
+      effectTitle: 'increase in suicide',
+      imageUrl:
+        'https://yaleclimateconnections.org/wp-content/uploads/2018/04/041718_child_factories.jpg',
     },
-    typography: {
-      textAlign: 'center',
+    {
+      effectDescription: 'No short desc available at present',
+      effectId: 'R8epBa4UvcieLTynfK3E84u',
+      effectScore: 14,
+      effectTitle: 'decrease in population of moose available to hunt',
+      imageUrl:
+        'https://yaleclimateconnections.org/wp-content/uploads/2018/04/041718_child_factories.jpg',
     },
-  });
-  
-  const ClimateFeed: React.FC = () => {
-    const classes = styles();
-    // const climateFeed = useClimateFeed();
-    const climateFeed = [
-      {
-        effectDescription: "No short desc available at present",
-        effectId: "R8t0oNsG3WgnupXsBVSjMHZ",
-        effectScore: 14,
-        effectTitle: "increase in suicide",
-        imageUrl: "https://yaleclimateconnections.org/wp-content/uploads/2018/04/041718_child_factories.jpg",
-      },
-      {
-        effectDescription: "No short desc available at present",
-        effectId: "R8epBa4UvcieLTynfK3E84u",
-        effectScore: 14,
-        effectTitle: "decrease in population of moose available to hunt",
-        imageUrl: "https://yaleclimateconnections.org/wp-content/uploads/2018/04/041718_child_factories.jpg",
-      }
-    ];
-  
-    if (!climateFeed || !climateFeed.length) {
-      return <Loader />;
-    }
-    return (
-      <>
-        {/* Toolbar required to prevent content disap behind the app bar*/}
+  ];
+
+  if (!climateFeed || !climateFeed.length) {
+    return <Loader />;
+  }
+  return (
+    <>
+      {/* Toolbar required to prevent content disap behind the app bar*/}
+      <Grid
+        container
+        className={classes.root}
+        data-testid="ClimateFeed"
+        justify="space-around"
+      >
+        <AppBar />
         <Toolbar variant="dense" />
-        <Grid
-          container
-          className={classes.root}
-          data-testid="ClimateFeed"
-          justify="space-around"
-        >
-          <Grid item sm={12} lg={12} container>
-            {climateFeed.map((effect, i) => (
-              <CMCard
-                key={`value-${i}`}
-                index={i}
-                title={effect.effectTitle}
-                shortDescription={effect.effectDescription}
-                numberedCards={false}
-                imageUrl={effect.imageUrl}
-                footer={
-                  <CMCardOverlay
-                    title={effect.effectTitle}
-                    imageUrl={effect.imageUrl}
-                    shortDescription={effect.effectDescription}
-                  />
-                }
-              />
-            ))}
-          </Grid>
-  
-          <Grid item sm={false} lg={4}>
-            {/* right gutter */}
-          </Grid>
+        <Grid item sm={12} lg={12} container>
+          {climateFeed.map((effect, i) => (
+            <CMCard
+              key={`value-${i}`}
+              index={i}
+              title={effect.effectTitle}
+              shortDescription={effect.effectDescription}
+              numberedCards={false}
+              imageUrl={effect.imageUrl}
+              footer={
+                <CMCardOverlay
+                  title={effect.effectTitle}
+                  imageUrl={effect.imageUrl}
+                  shortDescription={effect.effectDescription}
+                />
+              }
+            />
+          ))}
         </Grid>
-      </>
-    );
-  };
+
+        <Grid item sm={false} lg={4}>
+          {/* right gutter */}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default {
   title: 'ClimateMind/pages/ClimateFeedPage',
-  component: ClimateFeed
+  component: ClimateFeed,
 } as Meta;
 
 // use args to pass props. Compare with other stories...
@@ -94,4 +97,3 @@ const Template: Story<{}> = (args) => <ClimateFeed {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
-
