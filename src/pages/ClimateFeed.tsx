036f28567fbc2f7eaa984455c96ec3edfc9c6c14
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-11-27 22:39:03 +0000

    🔥 IT WORKS!!! 😎 🔥

--- src/pages/ClimateFeed.tsx
+++ src/pages/ClimateFeed.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, makeStyles, Toolbar } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import Loader from '../components/Loader';
 import CMCard from '../components/CMCard';
 import PageWrapper from '../components/PageWrapper';
