89442005739dd8502028576d9cc3c28b238d31e2
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-12-06 17:57:02 +0000

    more renaming

--- src/stories/components/CardHeadline.stories.tsx
+++ src/stories/components/CardHeadline.stories.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { Done } from "@material-ui/icons";
+import { Done } from '@material-ui/icons';
 
-import ActionHeadline, { ActionHeadlineProps } from '../../components/ActionHeadline';
+import ActionHeadline, {
+  ActionHeadlineProps,
+} from '../../components/CardHeadline';
 
 export default {
   title: 'ClimateMind/components/ActionHeadline',
   component: ActionHeadline,
 } as Meta;
 
-const Template: Story<ActionHeadlineProps> = (args) => <ActionHeadline {...args} />;
+const Template: Story<ActionHeadlineProps> = (args) => (
+  <ActionHeadline {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
@@ -24,5 +28,5 @@ Headline.args = {
 export const Icon = Template.bind({});
 Icon.args = {
   actionHeadline: 'Reducing Food Waste!',
-  icon:  <Done/>,
+  icon: <Done />,
 };
