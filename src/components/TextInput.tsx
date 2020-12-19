ae5bdc012cdb22c61f05af4cb8c4653837e438df
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-12-19 18:58:10 +0000

    Fix error label

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,6 @@
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { COLORS } from '../common/styles/CMTheme';
 
 const CMTextField = withStyles({
   root: {
@@ -8,6 +9,9 @@ const CMTextField = withStyles({
     },
     '& label.Mui-focused': {
       color: '#889a9c',
+    },
+    '& label.Mui-focused.Mui-error': {
+      color: COLORS.ERROR,
     },
     '& .MuiFilledInput-underline': {
       backgroundColor: '#FFF',
