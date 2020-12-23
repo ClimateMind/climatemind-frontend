fe1460ce51a1dbb34d44d4e04d516b1a3cba1727
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-12-23 18:23:49 +0000

    Add unit test for xipcode validation

--- src/pages/GetZipCode.tsx
+++ src/pages/GetZipCode.tsx
@@ -133,7 +133,7 @@ const GetZipCode: React.FC<{}> = () => {
         <Box mt={1}>
           <Typography variant="body1" align="center">
             <Button onClick={handleSkip} className={classes.skipButton}>
-              skip question
+              DON'T SHARE LOCATION
             </Button>
           </Typography>
         </Box>
