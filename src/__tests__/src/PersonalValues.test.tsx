36bc850f4734df00cc99a1e2a955d039965d589c
Daniel Michael <itzdanielmichael@gmail.com>
2021-01-10 22:50:41 -0500

    update PersonalValues.test.tsx

--- src/__tests__/src/PersonalValues.test.tsx
+++ src/__tests__/src/PersonalValues.test.tsx
@@ -44,7 +44,7 @@ describe('Climate Personality', () => {
     const { getByText } = render(<PersonalValues />);
     expect(
       getByText(
-        /You are about to see the effects of climate change and how you can take action against it/i
+        /Next Up, I will show you how you can take action against climate change./i
       )
     ).toBeInTheDocument();
   });
