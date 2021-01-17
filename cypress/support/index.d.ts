78d2e69dc6c4046d8bcc8376d2dd5fdc1ab47d6b
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-01-17 17:31:30 +0000

    add cy types for commands

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,21 +1,29 @@
 /// <reference types="cypress" />
 
-
 declare namespace Cypress {
   interface Chainable {
     /**
      * A command to check a pages accessibility
-    */
+     */
     checkAccessibility(log_type: any): void;
 
     /**
      * A command to go to next question
-    */
-   goToNextQuestion(): void;
+     */
+    goToNextQuestion(): void;
 
     /**
      * A command to go to previous question
-    */
-   goToPreviousQuestion(): void;
+     */
+    goToPreviousQuestion(): void;
+
+    /**
+     * A command to go set hasAcceptedCookies in localStorage
+     */
+    acceptCookies(): void;
+    /**
+     * A command to go set setSession in localStorage to 1234
+     */
+    setSession(): void;
   }
 }
