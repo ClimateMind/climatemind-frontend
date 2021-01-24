f0ae7229c3af1b398580e8124ac8ed939c2bf1dd
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-01-24 14:21:15 +0000

    Create <Paragraphs/> component

---
+++ src/components/Paragraphs.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+interface Props {
+  text: string;
+}
+
+// Component to take a text string and split it in \n and render as paragrapghs
+const Paragraphs: React.FC<Props> = ({ text }) => {
+  const paras = text.split('\n');
+  return (
+    <div>
+      {paras.map((p, i) => (
+        <Typography
+          variant="body1"
+          component="p"
+          key={i}
+          data-testid="paragraphs-p"
+        >
+          {p}
+        </Typography>
+      ))}
+    </div>
+  );
+};
+
+export default Paragraphs;
