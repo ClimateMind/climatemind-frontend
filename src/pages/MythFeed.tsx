b18b56e4b95fa4189a338a00bb5d6d0879a6b59b
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-03-17 20:51:15 +0000

    CM-529 update myth feed

--- src/pages/MythFeed.tsx
+++ src/pages/MythFeed.tsx
@@ -1,78 +1,37 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { getMyths } from '../api/getMyths';
 import { COLORS } from '../common/styles/CMTheme';
-import { Typography, Grid, makeStyles, Box } from '@material-ui/core';
 import Loader from '../components/Loader';
-import Error500 from './Error500';
 import MythCard from '../components/MythCard';
-import Wrapper from '../components/Wrapper';
+import PageContent from '../components/PageContent';
+import PageTitle from '../components/PageTitle';
 import ScrollToTopOnMount from '../components/ScrollToTopOnMount';
-
-const styles = makeStyles({
-  root: {
-    flexGrow: 1,
-    minHeight: '100vh',
-    paddingBottom: 24,
-  },
-  callToActionSection: {
-    minHeight: '100vh',
-  },
-  typography: {
-    textAlign: 'center',
-  },
-});
+import Wrapper from '../components/Wrapper';
+import Error500 from './Error500';
 
 const MythFeed: React.FC = () => {
-  const classes = styles();
-
   const { data, isLoading, error } = useQuery('myths', getMyths);
 
   if (error) return <Error500 />;
 
   return (
-    <Grid
-      container
-      className={classes.root}
-      data-testid="Myths Feed"
-      justify="space-around"
-    >
+    <>
+      <ScrollToTopOnMount />
       <Wrapper bgColor={COLORS.ACCENT4}>
-        <Grid item sm={false} lg={4}>
-          {/* left gutter */}
-        </Grid>
+        <PageContent>
+          <PageTitle>Climate Mind is against misinformation.</PageTitle>
 
-        <ScrollToTopOnMount />
-
-        <Grid
-          item
-          xs={12}
-          sm={10}
-          md={8}
-          lg={6}
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Box my={3} px={1}>
-            <Typography variant="h4">
-              Climate Mind is against misinformation.
-            </Typography>
-          </Box>
-
-          <Grid item sm={12} lg={12} container>
+          <Grid container>
             {isLoading && <Loader />}
             {data?.myths.map((myth, i) => (
               <MythCard myth={myth} key={i} />
             ))}
           </Grid>
-        </Grid>
-        <Grid item sm={false} lg={4}>
-          {/* right gutter */}
-        </Grid>
+        </PageContent>
       </Wrapper>
-    </Grid>
+    </>
   );
 };
 
