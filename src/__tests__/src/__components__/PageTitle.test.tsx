75c3305bd8ae75e72e7a8a9bd46b2a214059dcad
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-03-17 21:19:11 +0000

    fix console error in test

--- src/__tests__/src/__components__/PageTitle.test.tsx
+++ src/__tests__/src/__components__/PageTitle.test.tsx
@@ -4,15 +4,8 @@ import PageTitle from '../../../components/PageTitle';
 
 describe('Page Title', () => {
   it('It renders children', () => {
-    const { getByText } = render(
-      <PageTitle>
-        <h1>This is a title</h1>
-        <p>This is a paragraph</p>
-      </PageTitle>
-    );
+    const { getByText } = render(<PageTitle>This is a title</PageTitle>);
     const title = getByText('This is a title');
-    const paragraph = getByText('This is a paragraph');
     expect(title);
-    expect(paragraph);
   });
 });
