2656fcee71909254cc9a3985161023643a316218
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-03-17 00:19:33 +0000

    First pass at responsive heading

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Typography, Box } from '@material-ui/core';
+import { Typography, Box, useMediaQuery } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import theme from '../common/styles/CMTheme';
 
 const PageTitle: React.FC = ({ children }) => {
   const useStyles = makeStyles((theme) =>
@@ -9,14 +10,25 @@ const PageTitle: React.FC = ({ children }) => {
         textAlign: 'center',
         width: '100%',
       },
+      heading: {
+        fontSize: '40px',
+        [theme.breakpoints.up('sm')]: {
+          fontSize: '50px',
+        },
+        [theme.breakpoints.up('lg')]: {
+          fontSize: '60px',
+        },
+      },
     })
   );
+
+  const isXSmall = useMediaQuery(theme.breakpoints.down('xs'));
 
   const classes = useStyles();
 
   return (
-    <Box py={3} className={classes.root}>
-      <Typography variant="h3" component="h1">
+    <Box py={isXSmall ? 4 : 8} className={classes.root}>
+      <Typography className={classes.heading} variant="h1" component="h1">
         {children}
       </Typography>
     </Box>
