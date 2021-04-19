d7d8233b2a842d348c20308ff03b30ca32feb546
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-19 11:14:03 +0100

    tidyup

--- src/components/PersonalityChart.tsx
+++ src/components/PersonalityChart.tsx
@@ -1,56 +1,84 @@
 import { Box, useMediaQuery } from '@material-ui/core';
 import React from 'react';
-import { Polar } from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2';
 import theme, { COLORS } from '../common/styles/CMTheme';
 import PageTitle from '../components/PageTitle';
 import { useClimatePersonality } from '../hooks/useClimatePersonality';
 
 export default function PersonalityChart() {
-  // TODO: Add actual chart data
   const { valueScores } = useClimatePersonality();
   const data = valueScores?.map((value) => value.score);
   const labels = valueScores?.map((value) => value.personalValue);
-
+  const isXS = useMediaQuery(theme.breakpoints.down('sm'));
   const chartData = {
     datasets: [
       {
+        label: 'Your Personal Score',
         data: data,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.4)',
-          'rgba(54, 162, 235, 0.4)',
-          COLORS.ACCENT1,
-          COLORS.ACCENT2,
-          COLORS.ACCENT3,
-          COLORS.ACCENT4,
-          COLORS.ACCENT5,
-          COLORS.SUCCESS,
-          COLORS.PRIMARY,
-          COLORS.WARNING,
-        ],
       },
     ],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-
     labels,
   };
 
-  const isXS = useMediaQuery(theme.breakpoints.down('xs'));
+  const chartOptions = {
+    maintainAspectRatio: false,
+    legend: {
+      display: true,
+      position: 'bottom',
+    },
+    tooltips: {
+      callbacks: {
+        title: (e: any) => {
+          const event = e[0];
+          const valueName = labels[event.index];
+          return valueName.toLocaleUpperCase();
+        },
+      },
+    },
+    scale: {
+      gridLines: {
+        color: COLORS.PRIMARY,
+      },
+      angleLines: {
+        color: COLORS.PRIMARY,
+      },
+      ticks: {
+        beginAtZero: false,
+        backdropPaddingX: 4,
+        backdropPaddingY: 2,
+      },
+      pointLabels: {
+        fontColor: 'white',
+        fontSize: 13,
+        display: !isXS,
+      },
+    },
+    elements: {
+      point: {
+        radius: 6,
+        pointStyle: 'circle',
+        backgroundColor: COLORS.SECONDARY,
+        borderWidth: 2,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        hoverRadius: 40,
+        hitRadius: 10,
+        hoverBorderWidth: 5,
+      },
+      line: {
+        backgroundColor: 'rgba(184, 244,252, 0.4)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+      },
+    },
+  };
+
   return (
     <Box my={2} mb={4}>
       <PageTitle variant="h2">Your Personal Values</PageTitle>
       {chartData && labels && data && (
-        <Polar
+        <Radar
           data={chartData}
-          // width={isXS ? 100 : 800}
-          height={isXS ? 100 : 300}
-          options={{
-            maintainAspectRatio: false,
-            legend: {
-              display: true,
-              position: 'bottom',
-            },
-          }}
+          // width={!isXS ? 800 : 0}
+          options={chartOptions}
         />
       )}
     </Box>
