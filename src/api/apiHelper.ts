b369bdd083b484fffde2b4f4bbc6ff3c627110ae
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-06-10 21:23:44 +0100

    null check on apit host

--- src/api/apiHelper.ts
+++ src/api/apiHelper.ts
@@ -1,6 +1,11 @@
 // Takes an api endpoint and optional query pramam then retruns the correct url to call on Climate Mind API
 export const buildUrl = (endpoint: string) => {
   const API_HOST = process.env.REACT_APP_API_URL;
+
+  if (!API_HOST) {
+    throw new Error('No Api host defined');
+  }
+
   console.log({ API_HOST });
   const URL = `${API_HOST}${endpoint}`;
   return URL;
