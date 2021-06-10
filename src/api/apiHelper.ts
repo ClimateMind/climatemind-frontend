f9206879a5aca88b6eca5d23c3bfaac0ac045219
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-06-10 17:44:30 +0100

    console.log

--- src/api/apiHelper.ts
+++ src/api/apiHelper.ts
@@ -1,6 +1,7 @@
 // Takes an api endpoint and optional query pramam then retruns the correct url to call on Climate Mind API
 export const buildUrl = (endpoint: string) => {
   const API_HOST = process.env.REACT_APP_API_URL;
+  console.log({ API_HOST });
   const URL = `${API_HOST}${endpoint}`;
   return URL;
 };
