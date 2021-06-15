1a0fd45ba5b55b96b24531a46fa80526fe1bbbc2
Kameron Rodrigues <kameronr@stanford.edu>
2021-06-14 22:34:53 -0700

    trying out Solution2
    See
    https://www.freecodecamp.org/news/how-to-implement-runtime-environment-v
    ariables-with-create-react-app-docker-and-nginx-7f9d42a91d70/

--- serverdata.sh
+++ serverdata.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
 #!/usr/bin/env bash
-PATH=./public/index.html
+
+INDEXPATH=/usr/share/nginx/html/index.html
 JSON_STRING=$( jq -n --arg apiUrl "$REACT_APP_API_URL" '{REACT_APP_API_URL: $apiUrl}' )
-sed -i 's/__SERVERDATA__/$JSON_STRING/g' $PATH
+sed -i 's/__SERVERDATA__/$JSON_STRING/g' $INDEXPATH
 #awk '{sub("__SERVERDATA__",$JSON_STRING)}1' $1 > temp.txt && mv temp.txt $1
