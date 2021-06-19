f9adc7ab93a8f4ae155a670c93ab6e80ca59875e
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-06-19 15:47:29 +0100

    tidy up

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { TAuth } from '../types/Auth';
-import { useAuth } from '../hooks/auth/useAuth';
+// import { useAuth } from '../hooks/auth/useAuth';
 
 export type TAuthDispatch = React.Dispatch<React.SetStateAction<TAuth>>;
 
