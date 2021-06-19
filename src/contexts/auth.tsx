c141d3de43ac06e40ad18a3dc565c2df3dc3a8ac
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-06-19 16:17:50 +0100

    remove un-needed imports

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 import { TAuth } from '../types/Auth';
 
 export type TAuthDispatch = React.Dispatch<React.SetStateAction<TAuth>>;
