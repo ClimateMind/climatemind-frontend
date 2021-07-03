840f7f674a8d3aa29a9e080df692986aaeaf6e13
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-07-03 10:44:13 +0100

    update queryclient

--- src/contexts/queryClient.tsx
+++ src/contexts/queryClient.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { getMyths } from '../api/getMyths';
 import { getSolutions } from '../api/getSolutions';
@@ -6,34 +6,36 @@ import { getFeed } from '../api/getFeed';
 import { useSession } from '../hooks/useSession';
 
 // Query client provider to allow useQuery
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 300000, // 5 minutes
+export const QueryProvider: React.FC = ({ children }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 300000, // 5 minutes
+      },
     },
-  },
-});
+  });
 
-const QueryProvider: React.FC = ({ children }) => {
   const { sessionId } = useSession();
+  console.log({ sessionId });
 
-  // Prefetch myths
-  queryClient.prefetchQuery('myths', getMyths);
+  useEffect(() => {
+    // Prefetch myths
+    queryClient.prefetchQuery('myths', getMyths);
 
-  // Prefetch solutions when we have sessionId
-  queryClient.prefetchQuery(['solutions', sessionId], () => {
-    if (sessionId) {
-      return getSolutions(sessionId);
-    }
-  });
+    // Prefetch solutions when we have sessionId
+    queryClient.prefetchQuery(['solutions', sessionId], () => {
+      if (sessionId) {
+        return getSolutions(sessionId);
+      }
+    });
 
-  // Prefetch feed when we have sessionId
-  queryClient.prefetchQuery(['feed', sessionId], () => {
-    if (sessionId) {
-      return getFeed(sessionId);
-    }
-  });
-
+    // Prefetch feed when we have sessionId
+    queryClient.prefetchQuery(['feed', sessionId], () => {
+      if (sessionId) {
+        return getFeed(sessionId);
+      }
+    });
+  }, [sessionId, queryClient]);
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
