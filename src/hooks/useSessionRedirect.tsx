dbb263a82d538d454dd231e9eee0b6faa581e0ce
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-07-04 14:14:05 +0100

    dont redirect on sesion id any more

--- src/hooks/useSessionRedirect.tsx
+++ src/hooks/useSessionRedirect.tsx
@@ -3,6 +3,7 @@ import { useSession } from './useSession';
 import { useHistory } from 'react-router-dom';
 import ROUTES from '../components/Router/RouteConfig';
 
+// TODO: Remove or update this now session id is different. We no longer need to redirect on session id as there is always one as it is set on load. This
 export const useSessionRedirect = () => {
   const { push } = useHistory();
 
