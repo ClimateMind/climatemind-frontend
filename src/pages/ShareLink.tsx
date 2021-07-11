d7e4c1ee5c8219a353b47dfc177a1d9f91ec9a22
Johan Olsson <jawstiunda@gmail.com>
2021-07-11 10:25:07 +0200

    enter text input with Formik validation

--- src/pages/ShareLink.tsx
+++ src/pages/ShareLink.tsx
@@ -1,9 +1,8 @@
 import { Box, TextField, Typography } from '@material-ui/core';
+import { useFormik } from 'formik';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { ReactComponent as CMLogo } from '../assets/cm-logo-mint.svg';
-import { ReactComponent as UpArrowIcon } from '../assets/icon-arrow-up.svg';
 import { COLORS } from '../common/styles/CMTheme';
 import Button from '../components/Button';
 import ROUTES from '../components/Router/RouteConfig';
@@ -11,6 +10,8 @@ import ScrollToTopOnMount from '../components/ScrollToTopOnMount';
 import { useSessionRedirect } from '../hooks/useSessionRedirect';
 import { useBreakpoint } from '../hooks/useBreakpoint';
 import PageTitle from '../components/PageTitle';
+import TextInput from '../components/TextInput';
+import { generateLinkSchema } from '../helpers/validationSchemas';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -24,7 +25,7 @@ const useStyles = makeStyles(() =>
       gridTemplateRows: '1fr',
       justify: 'center',
       alignItems: 'center',
-      backgroundColor: COLORS.SECONDARY,
+      backgroundColor: COLORS.ACCENT7,
     },
     container: {
       textAlign: 'center',
@@ -39,26 +40,11 @@ const useStyles = makeStyles(() =>
     typography: {
       textAlign: 'center',
     },
-    bottomText: {
-      color: 'white',
-    },
     pageHeader: {
       marginTop: '1.3em',
     },
-    logo: {
-      paddingRight: '0.5em',
-    },
-    upArrow: {
-      marginLeft: '-14px',
-    },
     form: {
       width: '100%',
-    },
-    formInput: {
-      paddingRight: 8,
-    },
-    submit: {
-      textAlign: 'center',
     },
   })
 );
@@ -68,68 +54,57 @@ const ShareLink: React.FC<{}> = () => {
   const history = useHistory();
   const yPadding = 3; // Padding between boxes
 
+  const handleShareLink = (friend: string) => {
+    console.log({friend});
+  }
+
+  // Set initial form values and handle submission
+  const formik = useFormik({
+    initialValues: {
+      friend: '',
+    },
+    validationSchema: generateLinkSchema,
+    onSubmit: (values) => {
+      handleShareLink(values.friend);
+    },
+  });
+
   return (
     <div className={classes.root}>
       <section className={classes.section}>
         <div className={classes.container}>
-          {/* <Box mb={2} mt={5}>
-            <PageTitle variant="h1">Woah! Begin a conversation – starting here.</PageTitle>
-          </Box>
-
-          <Box textAlign="center">
-            <Typography variant="h6">
-              Share a link to the personal values survey with a friend or 
-              family member to get the conversation started.
-            </Typography>
-          </Box> */}
-
-          {/* <Box mt={5}>
-            <Button
-              variant="contained"
-              color="primary"
-              disableElevation
-              onClick={() => history.push(ROUTES.ROUTE_QUIZ)}
-            >
-              Generate Link
-            </Button>
-          </Box> */}
-
-          <form className={classes.form}>
+          
+          <form className={classes.form} onSubmit={formik.handleSubmit}>
             <Box py={yPadding}>
-              <TextField id="filled-basic" label="Filled" variant="filled" />
-              {/* <TextField
-                id="generateLinkInput"
-                // label="Add their name"
-                placeholder="Name to send to"
+              <TextInput
+                name="friend"
+                id="friend"
+                label="Name friend"
+                value={formik.values.friend}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                placeholder="hello@climatemind.org"
                 fullWidth={true}
+                error={formik.touched.friend && Boolean(formik.errors.friend)}
+                helperText={formik.touched.friend && formik.errors.friend}
                 variant="filled"
                 color="secondary"
-                value={''}
                 margin="none"
-                // onChange={handleInputChange}
-                // error={isInputError}
-                helperText={'text'}
-              /> */}
+              />
             </Box>
-            <Box component="div" className={classes.submit} py={yPadding}>
+            <Box component="div" textAlign="center" py={yPadding}>
               <Button
-                id="submitButton"
-                // disabled={!canSubmit}
-                color="primary"
-                // onClick={handleSubmit}
                 variant="contained"
+                disabled={!(formik.dirty && formik.isValid)}
+                color="primary"
+                onClick={() => formik.handleSubmit}
+                type="submit"
                 disableElevation
               >
                 Generate Link
               </Button>
             </Box>
           </form>
-
-          {/* <Box textAlign="center">
-            <Typography variant="h6">
-              And share with however you chat with your friend
-            </Typography>
-          </Box> */}
         </div>
       </section>
     </div>
