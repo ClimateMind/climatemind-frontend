870d75ece3b1d4b3fe31aefcc38ade5def16ad59
Johan Olsson <jawstiunda@gmail.com>
2021-07-29 14:11:26 +0200

    fix: added story for PageTitle and PersonalValuesFeed

---
+++ src/stories/pages/PersonalValues.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import PersonalValuesFeed from '../../pages/PersonalValuesFeed';
+import { MemoryRouter } from 'react-router-dom';
+
+export default {
+  title: 'ClimateMind/pages/PersonalValuesFeed',
+  component: PersonalValuesFeed,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<{}> = (args) => <PersonalValuesFeed {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
