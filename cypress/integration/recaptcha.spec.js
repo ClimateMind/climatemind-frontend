6d19ee5a28018751173b421fa7a34b85e6870455
SabineU <54753457+SabineU@users.noreply.github.com>
2021-08-15 19:33:49 +0200

    Test clicking Recaptcha
    Added script to Test clicking Recaptcha

--- cypress/integration/recaptcha.spec.js
+++ cypress/integration/recaptcha.spec.js
@@ -1,6 +1,6 @@
 ////To Test clicking recaptcha
 describe('Login With Recaptcha', () => {
-    it('should let you switch to the iframe', () => {
+    it('should let user click on recaptcha', () => {
         cy.visit('/login');
         cy.contains(/Accept/i).click({ force: true });
         cy.loginWithRecaptcha("anna@example.com", "@Anna1234!");
