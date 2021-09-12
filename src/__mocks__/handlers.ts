e1fd4705bb503cee4fbdc05c931e1a0a190b7b01
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-09-12 12:17:42 +0100

    get api as var

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -14,56 +14,53 @@ import register from './responses/register.json';
 import zipcode from './responses/zipCode.json';
 
 export const handlers = [
-  rest.post('http://localhost:5000/session', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(session));
-  }),
+  rest.post(
+    `${process.env.REACT_APP_API_URL}:5000/session`,
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(session));
+    }
+  ),
 
-  rest.get('http://localhost:5000/myths', (req, res, ctx) => {
+  rest.get(`${process.env.REACT_APP_API_URL}/myths`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(myths));
   }),
 
-  rest.get('http://localhost:5000/questions', (req, res, ctx) => {
+  rest.get(`${process.env.REACT_APP_API_URL}/questions`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(questions));
   }),
 
-  rest.post('http://localhost:5000/scores', (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_API_URL}/scores`, (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(scores));
   }),
 
   rest.get(
-    `http://localhost:5000/personal_values?quizId=${session.sessionId}`,
+    `${process.env.REACT_APP_API_URL}/personal_values`,
     (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(personalValues));
     }
   ),
 
-  rest.get(
-    `http://localhost:5000/feed?quizId=${session.sessionId}`,
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(climateFeed));
-    }
-  ),
+  rest.get(`${process.env.REACT_APP_API_URL}/feed`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(climateFeed));
+  }),
 
-  rest.get(
-    `http://localhost:5000/solutions?quizId=${session.sessionId}`,
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(solutions));
-    }
-  ),
+  rest.get(`${process.env.REACT_APP_API_URL}/solutions`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(solutions));
+  }),
 
-  rest.post(`http://localhost:5000/login`, (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_API_URL}/login`, (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(login));
   }),
 
-  rest.post(`http://localhost:5000/logout`, (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_API_URL}/logout`, (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(logout));
   }),
 
-  rest.post(`http://localhost:5000/register`, (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_API_URL}/register`, (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(register));
   }),
 
-  rest.post(`http://localhost:5000/post-code`, (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_API_URL}/post-code`, (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(zipcode));
   }),
 ];
