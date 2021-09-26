3a40a32147d23c9bd7dabb3018591f421ccc106f
Johan Olsson <jawstiunda@gmail.com>
2021-09-27 00:44:35 +0200

    added missing dependency in useEffect hook

--- src/contexts/session.tsx
+++ src/contexts/session.tsx
@@ -44,7 +44,7 @@ export const SessionProvider: React.FC = ({ children }) => {
       ...prevState,
       quizId: quizIdFromStorage,
     }));
-  }, [data]);
+  }, [data, quizIdFromStorage]);
 
   useEffect(() => {
     setSession((prevState) => ({
