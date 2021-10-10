0a9dc49604cce69b6531e3b83a91680565854eff
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-10-10 11:26:43 +0100

    hide disabled features

--- src/pages/ProfileMenu.tsx
+++ src/pages/ProfileMenu.tsx
@@ -1,4 +1,3 @@
-
 import { createStyles, Grid, makeStyles } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from 'react';
@@ -11,80 +10,91 @@ import UpdateEmailForm from '../components/UpdateEmailForm';
 import Wrapper from '../components/Wrapper';
 import { useAuth } from '../hooks/auth/useAuth';
 
-
 const ProfileMenu: React.FC = () => {
-    const { auth, logout } = useAuth();
-   
-    const [isPwdUpdateModal, setIsPwdUpdateModal] = React.useState(false)
-    const [isEmailUpdateModal, setIsEmailUpdateModal] = React.useState(false)
+  const { auth, logout } = useAuth();
 
-    const useStyles = makeStyles((theme) =>
+  const [isPwdUpdateModal, setIsPwdUpdateModal] = React.useState(false);
+  const [isEmailUpdateModal, setIsEmailUpdateModal] = React.useState(false);
+
+  const useStyles = makeStyles((theme) =>
     createStyles({
       profileMenuBtn: {
-        backgroundColor: "#fff",
+        backgroundColor: '#fff',
         border: `1px solid ${COLORS.SECONDARY}`,
         '&:FOCUS': {
-            backgroundColor: '#fff',
+          backgroundColor: '#fff',
         },
         '&:ACTIVE': {
-            backgroundColor: '#fff',
+          backgroundColor: '#fff',
         },
         paddingLeft: 10,
         paddingRight: 10,
       },
-      buttonText:{
+      buttonText: {
         paddingLeft: 5,
-      }
+      },
     })
   );
 
   const classes = useStyles();
-    
 
   const onLogout = () => {
-      logout()
-  }
+    logout();
+  };
 
+  const onConfirmPwdResetData = (values: any) => {
+    console.log('onConfirm', values);
+  };
 
-  const onConfirmPwdResetData = (values:any) => {
-    console.log("onConfirm",values);
-  } 
+  const onConfirmEmailUpdateData = (values: any) => {
+    console.log('onConfirm', values);
+  };
 
-  const onConfirmEmailUpdateData = (values:any) => {
-    console.log("onConfirm",values);
-  }
+  return (
+    <>
+      <Wrapper bgColor={COLORS.ACCENT8} fullHeight>
+        {auth?.isLoggedIn ? (
+          <PageContent>
+            <ChangePasswordForm
+              handleClose={() => setIsPwdUpdateModal(false)}
+              onConfirm={onConfirmPwdResetData}
+              isOpenModal={isPwdUpdateModal}
+            />
+            <UpdateEmailForm
+              handleClose={() => setIsEmailUpdateModal(false)}
+              onConfirm={onConfirmEmailUpdateData}
+              isOpenModal={isEmailUpdateModal}
+            />
 
-    return (
-        <>
-        <Wrapper bgColor={COLORS.ACCENT8} fullHeight>
-        {auth?.isLoggedIn ? <PageContent>
-              <ChangePasswordForm handleClose={() => setIsPwdUpdateModal(false)} onConfirm={onConfirmPwdResetData} isOpenModal={isPwdUpdateModal}/>
-              <UpdateEmailForm handleClose={() => setIsEmailUpdateModal(false)} onConfirm={onConfirmEmailUpdateData}  isOpenModal={isEmailUpdateModal} />
-
-              <PageTitle align="left">{auth?.firstName ? `${auth?.firstName}'s account` : ""}</PageTitle>
-              <Grid
-                container
-                direction="column"
-                justify="flex-start"
-                alignItems="flex-start"
-                spacing={2}
-                >
-                 <Grid item>
+            <PageTitle align="left">
+              {auth?.firstName ? `${auth?.firstName}'s account` : ''}
+            </PageTitle>
+            <Grid
+              container
+              direction="column"
+              justify="flex-start"
+              alignItems="flex-start"
+              spacing={2}
+            >
+              {/* TODO: Uncomment this when we can update email and password */}
+              {/* <Grid item>
                     <CMButton onClick = {() => setIsPwdUpdateModal(true)} className={classes.profileMenuBtn}>CHANGE PASSWORD</CMButton>
                  </Grid>
                  <Grid item>
                     <CMButton onClick = {() => setIsEmailUpdateModal(true)} className={classes.profileMenuBtn}>UPDATE EMAIL</CMButton>
-                 </Grid>
-                 <Grid item>
-                    <CMButton onClick = {onLogout} className={classes.profileMenuBtn}>
-                      <ExitToAppIcon/> <span className={classes.buttonText}>LOGOUT</span>
-                    </CMButton>
-                 </Grid>
+                 </Grid> */}
+              <Grid item>
+                <CMButton onClick={onLogout} className={classes.profileMenuBtn}>
+                  <ExitToAppIcon />{' '}
+                  <span className={classes.buttonText}>LOGOUT</span>
+                </CMButton>
               </Grid>
-            </PageContent> : null}
-        </Wrapper>
-        </>
-    )
-}
+            </Grid>
+          </PageContent>
+        ) : null}
+      </Wrapper>
+    </>
+  );
+};
 
 export default ProfileMenu;
