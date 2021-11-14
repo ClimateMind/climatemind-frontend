cf44aafccf7030f5e7113c80cc4bcc870b35c39a
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-11-14 14:39:29 +0000

    remove empty interface

--- src/components/PageContentFlex.tsx
+++ src/components/PageContentFlex.tsx
@@ -4,10 +4,6 @@ import React from 'react';
 import theme from '../common/styles/CMTheme';
 import ScrollToTopOnMount from '../components/ScrollToTopOnMount';
 
-interface PageContentProps {
-  
-}
-
 const PageContent: React.FC = ({ children }) => {
   const useStyles = makeStyles((theme) =>
     createStyles({
