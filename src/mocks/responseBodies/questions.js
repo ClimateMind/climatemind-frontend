1987f2a5959215486ddf8ef60e1eba37807c255b
NickCallaghan <nicholas.callaghan@gmail.com>
2021-11-19 21:59:21 +0000

    update resppnses

--- src/mocks/responseBodies/questions.js
+++ src/mocks/responseBodies/questions.js
@@ -3,99 +3,99 @@ export const QUESTIONS_RESPONSE = {
         {
             "id": 1,
             "value": "conformity",
-            "question": "You believe you should always show respect to your parents and to older people.\n\n It is important to you to be obedient."
+            "question": "MOCKED: You believe you should always show respect to your parents and to older people.\n\n It is important to you to be obedient."
         },
         {
             "id": 2,
             "value": "tradition",
-            "question": "Religious belief is important to you.\n\n You want your faith or family traditions to guide your decisions and be a core part of your life."
+            "question": "MOCKED: Religious belief is important to you.\n\n You want your faith or family traditions to guide your decisions and be a core part of your life."
         },
         {
             "id": 3,
             "value": "benevolence",
-            "question": "It’s very important to you to help the people around you.\n\n You want to care for their well-being."
+            "question": "MOCKED: It’s very important to you to help the people around you.\n\n You want to care for their well-being."
         },
         {
             "id": 4,
             "value": "universalism",
-            "question": "You think it is important that every person in the world be treated equally.\n\n You believe everyone should have equal opportunities in life."
+            "question": "MOCKED: You think it is important that every person in the world be treated equally.\n\n You believe everyone should have equal opportunities in life."
         },
         {
             "id": 5,
             "value": "self-direction",
-            "question": "You think it’s important to be interested in things.\n\n You like to be curious and to try to understand all sorts of things."
+            "question": "MOCKED: You think it’s important to be interested in things.\n\n You like to be curious and to try to understand all sorts of things."
         },
         {
             "id": 6,
             "value": "stimulation",
-            "question": "You like to take risks. \n\n You are always looking for adventures."
+            "question": "MOCKED: You like to take risks. \n\n You are always looking for adventures."
         },
         {
             "id": 7,
             "value": "hedonism",
-            "question": "You seek every chance you can to have fun. \n\n It is important to you to do things that give you pleasure."
+            "question": "MOCKED: You seek every chance you can to have fun. \n\n It is important to you to do things that give you pleasure."
         },
         {
             "id": 8,
             "value": "achievement",
-            "question": "Getting ahead in life is important to you.\n\n You strive to do better than others."
+            "question": "MOCKED: Getting ahead in life is important to you.\n\n You strive to do better than others."
         },
         {
             "id": 9,
             "value": "power",
-            "question": "You always want to be the one who makes the decisions.\n\n You like to be the leader."
+            "question": "MOCKED: You always want to be the one who makes the decisions.\n\n You like to be the leader."
         },
         {
             "id": 10,
             "value": "security",
-            "question": "Having a stable government is important to you.\n\n You are concerned that the social order is protected."
+            "question": "MOCKED: Having a stable government is important to you.\n\n You are concerned that the social order is protected."
         }
     ],
     "SetTwo": [
         {
             "id": 1,
             "value": "conformity",
-            "question": "It is important to you to always behave properly.\n\n You want to avoid doing anything people would say is wrong."
+            "question": "MOCKED: It is important to you to always behave properly.\n\n You want to avoid doing anything people would say is wrong."
         },
         {
             "id": 2,
             "value": "tradition",
-            "question": "You think it is best to do things in traditional ways.\n\n It is important to you to do things the way you were taught to do them."
+            "question": "MOCKED: You think it is best to do things in traditional ways.\n\n It is important to you to do things the way you were taught to do them."
         },
         {
             "id": 3,
             "value": "benevolence",
-            "question": "It is important to you to respond to the needs of others.\n\n You try to support those you know."
+            "question": "MOCKED: It is important to you to respond to the needs of others.\n\n You try to support those you know."
         },
         {
             "id": 4,
             "value": "universalism",
-            "question": "You believe all the worlds’ people should live in harmony.\n\n Promoting peace among all groups in the world is important to you."
+            "question": "MOCKED: You believe all the worlds’ people should live in harmony.\n\n Promoting peace among all groups in the world is important to you."
         },
         {
             "id": 5,
             "value": "self-direction",
-            "question": "Thinking up new ideas and being creative is important to you.\n\n You like to do things in your own original way."
+            "question": "MOCKED: Thinking up new ideas and being creative is important to you.\n\n You like to do things in your own original way."
         },
         {
             "id": 6,
             "value": "stimulation",
-            "question": "You think it is important to do lots of different things in life.\n\n You always looks for new things to try."
+            "question": " MOCKED: You think it is important to do lots of different things in life.\n\n You always looks for new things to try."
         },
         {
             "id": 7,
             "value": "hedonism",
-            "question": "You really want to enjoy life.\n\n Having a good time is very important to you."
+            "question": "MOCKED: You really want to enjoy life.\n\n Having a good time is very important to you."
         },
         {
             "id": 8,
             "value": "achievement",
-            "question": "Being very successful is important to you.\n\n You like to impress other people."
+            "question": "MOCKED: Being very successful is important to you.\n\n You like to impress other people."
         },
         {
             "id": 9,
             "value": "power",
-            "question": "It is important to you to be in charge and tell others what to do.\n\n You want people to do what you say."
+            "question": " MOCKED:It is important to you to be in charge and tell others what to do.\n\n You want people to do what you say."
         },
         {
             "id": 10,
