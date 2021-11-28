ce18afa5f372bba181d1112078f8deb9520ffb89
Nick Callaghan <nicholas.callaghan@xdesign.com>
2021-11-28 21:18:56 +0000

    Button

--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Wrapper from '../../components/Wrapper';
 import Button from '../../components/Button';
-import { COLORS } from '../../common/styles/CMTheme';
-import OpenInNew  from '@material-ui/icons/OpenInNew';
+import { ButtonProps } from '@material-ui/core';
+import OpenInNew from '@material-ui/icons/OpenInNew';
+import { StoryWrapper } from '../../components/StoryWrapper';
 
 export default {
   title: 'ClimateMind/components/Button',
   component: Button,
 } as Meta;
 
-const Template: Story<{}> = (args) => (
-  <Wrapper bgColor={COLORS.ACCENT3} fullHeight>
+const Template: Story<ButtonProps> = (args) => (
+  <StoryWrapper>
     <Button {...args}>Button</Button>
-  </Wrapper>
+  </StoryWrapper>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  variant: 'contained',
   color: 'primary',
   disableElevation: true,
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  ...Default.args,
+  variant: 'contained',
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  ...Default.args,
+  variant: 'contained',
+  color: 'secondary',
 };
 
 export const Disabled = Template.bind({});
@@ -33,7 +46,18 @@ Disabled.args = {
 export const InlineIcon = Template.bind({});
 InlineIcon.args = {
   ...Default.args,
-  disabled: true,
+  variant: 'contained',
+  endIcon: (
+    <>
+      <OpenInNew fontSize="small" />
+    </>
+  ),
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  ...Default.args,
+  variant: 'outlined',
   endIcon: (
     <>
       <OpenInNew fontSize="small" />
