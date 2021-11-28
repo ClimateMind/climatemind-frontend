63da580195863971ced79e37d7b8c73f9ca4c0eb
Nick Callaghan <nicholas.callaghan@xdesign.com>
2021-11-28 20:59:38 +0000

    Bottom Menu

--- src/stories/components/BottomMenu.stories.tsx
+++ src/stories/components/BottomMenu.stories.tsx
@@ -1,10 +1,11 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/react/types-6-0';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
-import Wrapper from '../../components/Wrapper';
-import { COLORS } from '../../common/styles/CMTheme';
-import BottomMenu, { BottomMenuProps } from '../../components/BottomMenu';
+import BottomMenu, {
+  bottomMenuLinks,
+  BottomMenuProps,
+} from '../../components/BottomMenu';
 
 export default {
   title: 'ClimateMind/components/BottomMenu',
@@ -12,54 +13,27 @@ export default {
   decorators: [
     (Story) => (
       <MemoryRouter>
-        <Wrapper bgColor={COLORS.SECONDARY} fullHeight>
+        <div
+          style={{
+            width: '100%',
+            height: '100vh',
+            backgroundColor: 'lightgrey',
+          }}
+        >
           <Story />
-        </Wrapper>
+        </div>
       </MemoryRouter>
     ),
   ],
 } as Meta;
 
-const fourBottomButtons = [
-  {
-    label: 'Feed',
-    value: 'climate-feed',
-    index: 1,
-  },
-  {
-    label: 'Myths',
-    value: 'myths',
-    index: 2,
-  },
-  {
-    label: 'Solutions',
-    value: 'solutions',
-    index: 3,
-  },
-  {
-    label: 'Saved',
-    value: 'saved',
-    index: 4,
-  },
-];
-
-const defaultButtons = [
-  fourBottomButtons[0],
-  fourBottomButtons[1],
-  fourBottomButtons[2],
-];
-const twoButtons = [fourBottomButtons[0], fourBottomButtons[1]];
+const twoButtons = [bottomMenuLinks[0], bottomMenuLinks[1]];
 
 const Template: Story<BottomMenuProps> = (args) => <BottomMenu {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  links: defaultButtons,
-};
-
 export const WithFourButtons = Template.bind({});
 WithFourButtons.args = {
-  links: fourBottomButtons,
+  links: bottomMenuLinks,
 };
 
 export const WithTwoButtons = Template.bind({});
