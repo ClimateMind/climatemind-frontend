adb68b13321a750e986b45d60faf93c9a7c1a397
NickCallaghan <nicholas.callaghan@gmail.com>
2021-11-30 19:53:29 +0000

    update cypress tests

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -41,15 +41,16 @@ declare namespace Cypress {
      */
     switchToIframe(iframeName: any): any;
 
-
     /**
      * A command to login with the recaptcha clicked
-    */
+     */
     loginWithRecaptcha(email: string, password: string): void;
 
     /**
      * A command to click the login button
      */
     clickLoginButton(): void;
+
+    answerFirstTenQuestions(): void;
   }
 }
