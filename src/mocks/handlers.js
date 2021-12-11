b47ecaaacc46338b22a340dbc3805df2fc14901c
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-12-11 10:14:08 +0000

    add new handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,6 +4,8 @@ import { PERSONAL_VALUES_RESPONSE } from './responseBodies/personalValuesResopns
 import { CONVERSATIONS_RESPONSE } from './responseBodies/conversationsResponse';
 import { QUESTIONS_RESPONSE } from './responseBodies/questions';
 import { GET_SINGLE_CONVERSATION_RESPONSE } from './responseBodies/getSingleConversationResponse';
+import { POST_ALIGNMENT_RESPONSE } from './responseBodies/postAlignment';
+import { GET_ALIGNMENT_RESPONSE } from './responseBodies/getAlignment';
 
 export const handlers = [
   // Capture a GET /user/:userId request,
@@ -27,24 +29,41 @@ export const handlers = [
     return res(ctx.json(MYTH_RESPONSE));
   }),
 
-  // TODO: Fix this scores should be a post
-  // rest.get('http://localhost:5000/scores', (req, res, ctx) => {
-  //   return res(
-  //     ctx.json({
-  //       sessionId: '52a95263-a95c-4dd1-85ab-6cd8013dce0d',
-  //     })
-  //   );
-  // }),
+  // POST Scores
+  rest.post('http://localhost:5000/scores', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        sessionId: '52a95263-a95c-4dd1-85ab-6cd8013dce0d',
+      })
+    );
+  }),
   rest.get('http://localhost:5000/conversations', (req, res, ctx) => {
     ctx.status(200);
     return res(ctx.json(CONVERSATIONS_RESPONSE));
   }),
+
+  // *** USER B JOURNEY***
+
+  // GET Single Conversaionion
   rest.get(
     /http:\/\/localhost:5000\/conversation\/[\w-]+/i,
     (req, res, ctx) => {
-      console.log('MOCKED GET myths');
+      console.log('MOCKED GET signle conversation');
       ctx.status(200);
       return res(ctx.json(GET_SINGLE_CONVERSATION_RESPONSE));
     }
   ),
+  // POST Alignmeent
+  rest.post(/http:\/\/localhost:5000\/alignment/i, (req, res, ctx) => {
+    console.log('MOCKED POST Alignment');
+    ctx.status(200);
+    return res(ctx.json(POST_ALIGNMENT_RESPONSE));
+  }),
+
+  // GET Alignment
+  rest.get(/http:\/\/localhost:5000\/alignment\/[\w-]+/i, (req, res, ctx) => {
+    console.log('MOCKED GET Alignment');
+    ctx.status(200);
+    return res(ctx.json(GET_ALIGNMENT_RESPONSE));
+  }),
 ];
