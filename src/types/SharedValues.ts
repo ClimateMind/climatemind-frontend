e727be01aa9e8c7b42b5289bf0b3404b9bf78d85
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-12-12 21:40:22 +0000

    add types

--- src/types/SharedValues.ts
+++ src/types/SharedValues.ts
@@ -1,1 +1,8 @@
-export interface SharedValues {}
+import { TPersonalValue } from './PersonalValues';
+
+export interface TSharedValues {
+  alignmentScore: [TPersonalValue];
+  overallSimilarityScore: number;
+  userAName: string;
+  userBName: string;
+}
