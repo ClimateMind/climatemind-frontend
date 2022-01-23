7901cc8c2f0511c25eea3711561c3e91ac3bf3a3
Nick Callaghan <nicholas.callaghan@gmail.com>
2022-01-23 10:22:05 +0000

    make existing cypress tests pass

--- src/mocks/responseBodies/getSingleConversationResponse.js
+++ src/mocks/responseBodies/getSingleConversationResponse.js
@@ -1,4 +1,14 @@
 export const GET_SINGLE_CONVERSATION_RESPONSE = {
-  conversationId: '2154508a-20a8-403d-b35f-c3610b605a84',
-  alignmentId: 'beb67e4a-c233-4ea7-85e0-5d6ed0483255',
+  consent: false,
+  conversationId: '8CC3F52E-88E7-4643-A490-519E170DB470',
+  conversationStatus: 0,
+  conversationTimestamp: 'Sun, 16 Jan 2022 17:39:10 GMT',
+  userA: {
+    id: 'BC48D45A-E542-4E22-B1BC-7F48BFBADE5E',
+    name: 'Nick',
+    sessionId: '353DCA14-CC15-4500-9C79-3855BF3C7D7D',
+  },
+  userB: {
+    name: 'Bill',
+  },
 };
