b6bdf8e3b689b278e0fe4f3f587ff87e8dfc2265
Nick Callaghan <nicholas.callaghan@gmail.com>
2022-08-02 17:42:06 +0100

    Create new page

---
+++ src/pages/userB/UserBNoConsent/UserBNoConsent.stories.tsx
@@ -0,0 +1,24 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { StoryBookProviders } from '../../../stories/utils/StoryBookProviders';
+import { NoConsent } from './UserBNoConsent';
+
+export default {
+  title: 'ClimateMind/pages/userB/NoConsent',
+  component: NoConsent,
+  decorators: [
+    (Story) => (
+      <StoryBookProviders>
+        <Story />
+      </StoryBookProviders>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<{}> = (args) => <NoConsent {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {};
+
+export const Default = Template.bind({});
---
+++ src/pages/userB/UserBNoConsent/UserBNoConsent.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import PageTitle from '../../../components/PageTitle';
+import { useHistory, Link } from 'react-router-dom';
+import { Grid, Box, Typography, makeStyles } from '@material-ui/core';
+import PageWrapper from '../../../components/PageWrapper';
+import { COLORS } from '../../../common/styles/CMTheme';
+import { Button } from '../../../components/Button';
+
+const styles = makeStyles((theme) => {
+  return {
+    root: {},
+    emoji: {
+      fontSize: '60px',
+      fontWeight: 900,
+      marginTop: '15vh',
+    },
+    title: {
+      fontSize: '36px',
+      fontWeight: 900,
+      margin: '1em 0',
+    },
+    message: { fontSize: '20px', fontWeight: 100 },
+    buttonDiv: {
+      textAlign: 'center',
+    },
+    textButton: {
+      color: COLORS.DK_TEXT,
+      marginTop: '1em',
+    },
+    links: {
+      textDecoration: 'none',
+      color: COLORS.SECONDARY,
+    },
+  };
+});
+
+export const NoConsent: React.FC<{}> = () => {
+  const classes = styles();
+  const { push } = useHistory();
+
+  const sendEmail = () => {
+    window.open('mailto:hello@climatemind.org');
+  };
+
+  return (
+    <PageWrapper bgColor={COLORS.PRIMARY}>
+      <Grid item>
+        <Box px={5}>
+          <PageTitle>No Problem</PageTitle>
+
+          <Typography
+            variant="body1"
+            align="center"
+            className={classes.message}
+          >
+            the page that was requested can't be found, but you could visit our{' '}
+            <Link className={classes.links} to="/">
+              Homepage
+            </Link>
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item className={classes.buttonDiv}>
+        <Grid item container justifyContent="center" direction="column">
+          <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            onClick={() => push('/')}
+          >
+            Go to homepage
+          </Button>
+
+          <Button
+            variant="text"
+            color="secondary"
+            disableElevation
+            className={classes.textButton}
+            onClick={() => sendEmail()}
+          >
+            Get Help / Contact Us
+          </Button>
+        </Grid>
+      </Grid>
+    </PageWrapper>
+  );
+};
