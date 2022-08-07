00b0a584a0c2f3fe5aa11ed90c0c2596953168d9
Nick Callaghan <nicholas.callaghan@gmail.com>
2022-08-07 20:37:49 +0100

    Fix Styles

--- src/components/ConversationCardUserBName/ConversationCardUserBName.tsx
+++ src/components/ConversationCardUserBName/ConversationCardUserBName.tsx
@@ -42,7 +42,7 @@ export const ConversationCardUserBName: React.FC<
     //eslint-disable-next-line
   }, [isEditable]);
 
-  // Return the name type when for not editable
+  // Return the name type when component is not editable not editable - this is used to trigger collapse from the componsent above.
   if (!isEditable)
     return (
       <Grid style={{ display: 'flex' }}>
@@ -55,33 +55,56 @@ export const ConversationCardUserBName: React.FC<
   // Return the form
   return (
     <form onSubmit={handleSubmit}>
-      {isEditable && isEditing ? (
-        <>
-          <TextInput
-            id="newUserBName"
-            name="receiver_name"
-            value={name}
-            onChange={(e) => setName(e.currentTarget.value)}
-            fullWidth={false}
-            variant="filled"
-            color="secondary"
-            margin="none"
-            data-cy="update-name-textfield"
-          />
-          <Button type="submit" aria-label="update name">
-            <EditIcon />
-          </Button>
-        </>
-      ) : (
-        <Grid style={{ display: 'flex' }}>
-          <Typography variant="h4" component="h4">
-            {capitalize(name)}
-          </Typography>
-          <Button type="submit" aria-label="edit name">
-            <EditIcon />
-          </Button>
-        </Grid>
-      )}
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        {isEditable && isEditing ? (
+          <>
+            <Grid item>
+              <TextInput
+                id="newUserBName"
+                name="receiver_name"
+                value={name}
+                onChange={(e) => setName(e.currentTarget.value)}
+                fullWidth={false}
+                variant="filled"
+                color="secondary"
+                margin="none"
+                data-cy="update-name-textfield"
+              />
+            </Grid>
+            <Grid item style={{ marginTop: 'auto' }}>
+              <Button type="submit" aria-label="update name">
+                <EditIcon />
+              </Button>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item>
+              <Typography
+                variant="h4"
+                component="h4"
+                style={{ display: 'inline-block' }}
+              >
+                {capitalize(name)}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                type="submit"
+                aria-label="edit name"
+                style={{ display: 'flex' }}
+              >
+                <EditIcon />
+              </Button>
+            </Grid>
+          </>
+        )}
+      </Grid>
     </form>
   );
 };
