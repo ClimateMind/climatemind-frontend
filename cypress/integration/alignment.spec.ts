9a402ab956ea29ef13b0534446b4b7aff2a348f5
Svenstar74 <sven.firmbach@gmx.de>
2022-08-14 18:10:52 +0200

    Mock api call in cypress test

--- cypress/integration/alignment.spec.ts
+++ cypress/integration/alignment.spec.ts
@@ -5,7 +5,23 @@ import { isFeatureEnabled } from '../../src/features';
 import ROUTES from '../../src/components/Router/RouteConfig';
 
 const conversationsEnabled = isFeatureEnabled.conversations;
-const conversartionId = '62cddb66-6c68-4601-8d8e-80fa496a280b';
+const conversationId = '62cddb66-6c68-4601-8d8e-80fa496a280b';
+
+const getSingleConversationResponse = {
+  conversationId: "62cddb66-6c68-4601-8d8e-80fa496a280b",
+  userA: {
+    name: "Bill",
+    id: "ba5df442-7261-4fc1-bff0-5dfd84035d56",
+    sessionId: "671e4949-a3e4-4844-b9d2-cd843f48f357"
+  },
+  userB: {
+    name: "Bob"
+  },
+  state: 2,
+  consent: true,
+  conversationTimestamp: "Sun, 10 Oct 2021 18:35:02 GMT",
+  alignmentScoresId: "842a4949-a3e4-4914-c9d2-cd843f48f357"
+};
 
 conversationsEnabled &&
   describe('Alignment', () => {
@@ -16,12 +32,20 @@ conversationsEnabled &&
     });
 
     it('allows navigation to selected topics', () => {
+
+      // Mock the api routes to return dummy data
+      cy.route({
+        method: 'GET',
+        url: /62cddb66-6c68-4601-8d8e-80fa496a280b/i,
+        response: getSingleConversationResponse,
+      });
+
       cy.login();
-      cy.visit(`${ROUTES.SHARED_VALUES}/${conversartionId}`);
+      cy.visit(`${ROUTES.SHARED_VALUES}/${conversationId}`);
       cy.contains(/VIEW SELECTED TOPICS/i).click();
       cy.url().should(
         'contain',
-        `${ROUTES.USERA_SHARED_FEED}/${conversartionId}`
+        `${ROUTES.USERA_SHARED_FEED}/${conversationId}`
       );
       cy.go('back');
     });
