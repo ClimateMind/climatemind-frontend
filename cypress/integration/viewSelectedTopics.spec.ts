f7396798575df828d39605e65c4415969eb79c94
Chris <chris.alafaa@gmail.com>
2022-08-26 19:03:16 +0100

    Update viewSelectedTopics.spec.ts

--- cypress/integration/viewSelectedTopics.spec.ts
+++ cypress/integration/viewSelectedTopics.spec.ts
@@ -1,1 +1,8 @@
 /// <reference types="cypress" />
+
+import { terminalLog } from '../support/helpers';
+
+it('It shows the selected topics', () => {
+    // cy.get('View Selected Topics').click()
+    cy.url().should('include', )
+})
