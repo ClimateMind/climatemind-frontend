743be18482429aa750f37946c7f3ec6eefe21ba6
Chris <chris.alafaa@gmail.com>
2022-08-29 06:22:24 +0100

    Cypress Tests for ViewSelectedTopics

--- cypress/integration/viewSelectedTopics.spec.ts
+++ cypress/integration/viewSelectedTopics.spec.ts
@@ -2,7 +2,16 @@
 
 import { terminalLog } from '../support/helpers';
 
-it('It shows the selected topics', () => {
-    // cy.get('View Selected Topics').click()
-    cy.url().should('include', )
+describe('View Selected Topics Page', () => {
+    beforeEach(() => {
+        cy.acceptCookies();
+        cy.mockServer();
+        cy.login();
+        cy.visit('/user-a-shared-feed/9F20CC7C-C0F5-47B3-B235-33CBE0755251');
+    })
+
+    it('It has the correct url', () => {
+        // cy.get('View Selected Topics').click()
+        cy.url().should('include', )
+    })
 })
