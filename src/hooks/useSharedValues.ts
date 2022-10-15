edee897a88a73d0639fd25a2818f71430e10c5b3
Nikolas Brix <nsbrix@gmail.com>
2022-10-14 19:01:03 -0700

    added useQuery dependency

--- src/hooks/useSharedValues.ts
+++ src/hooks/useSharedValues.ts
@@ -1,15 +1,21 @@
 import { useQuery } from 'react-query';
 import { getAlignment } from '../api/getAlignment';
-import { useAlignment } from './useAlignment';
+import { useGetOneConversation } from './useGetOneConversation';
+import { useUserB } from './useUserB';
 
 export function useSharedValues() {
-  const { alignmentScoresId } = useAlignment();
+  const { conversationId } = useUserB();
+  const { conversation } = useGetOneConversation(conversationId);
 
   return useQuery(
-    ['conversations', alignmentScoresId],
-    () => getAlignment(alignmentScoresId),
+    ['conversations', conversation?.alignmentScoresId],
+    () => {
+      if (conversation?.alignmentScoresId)
+        return getAlignment(conversation?.alignmentScoresId);
+    },
     {
       staleTime: 1000,
+      enabled: !!conversation?.alignmentScoresId,
     }
   );
 }
