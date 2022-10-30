2dc70c040cc56eb96519ac0f960f4fd501be6b78
Svenstar74 <sven.firmbach@gmx.de>
2022-10-30 11:03:24 +0100

    fix issues

--- src/hooks/useGetSessionId.ts
+++ src/hooks/useGetSessionId.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { postSession } from '../api/postSession';
 import { useSession } from './useSession';
@@ -6,36 +6,39 @@ import { useSessionStorage } from './useSessionStorage';
 import { useToast } from './useToast';
 
 export function useGetSessionId() {
-  const [storedSession, storeValue] = useSessionStorage(
-    '' as string,
-    'sessionId'
-  );
 
   const { showToast } = useToast();
+  const [sessionId, setSessionId] = useState<string>('');
   const { sessionState, setSessionState } = useSession();
 
-  const { mutateAsync } = useMutation(() => postSession(), {
-    onSuccess: (data) => {
-      storeValue(data.sessionId);
+  const getNewSessionId = () => {
+    if (sessionStorage.getItem('sessionId')) {
+      setSessionId(sessionStorage.getItem('sessionId')!);
       setSessionState('active');
-    },
-    onError: () => {
-      showToast({
-        type: 'error',
-        message:
-          'Something went wrong intializing your session. :( Please refresh the page to try again.',
-      });
-    },
-  });
-
-  // TODO: get rid of mutatesync and setSessionState dependencies. Causes infinite loop if included in dependency array.
-  useEffect(() => {
-    if (!storedSession && sessionState !== 'loading') {
-      setSessionState('loading');
-      mutateAsync();
+    } else {
+      if (sessionState !== 'new') {
+        setSessionState('loading');
+        postSession()
+          .then(res => {
+            setSessionId(res.sessionId)
+            setSessionState('active');
+            sessionStorage.setItem('sessionId', res.sessionId);
+          })
+          .catch(err => {
+            showToast({
+              type: 'error',
+              message:
+                'Something went wrong intializing your session. :( Please refresh the page to try again.',
+            });
+          })
+      }
     }
-    // eslint-disable-next-line
-  }, [storedSession, sessionState]);
+  }
 
-  return { sessionId: storedSession, getNewSession: mutateAsync };
+  useEffect(() => {
+    getNewSessionId();
+    // eslint-disable-next-line
+  }, []);
+
+  return { sessionId, getNewSessionId };
 }
