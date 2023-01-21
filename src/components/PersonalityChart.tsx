6ec4ad353c3a459c69688200867126b5a41b5a7f
Svenstar74 <sven.firmbach@gmx.de>
2023-01-21 19:04:07 +0100

    fix linting issue

--- src/components/PersonalityChart.tsx
+++ src/components/PersonalityChart.tsx
@@ -1,4 +1,4 @@
-//eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { Box } from '@material-ui/core';
 import React from 'react';
