f0e013fc270c076c00c3921b4f9ca20a32d680e4
Svenstar74 <sven.firmbach@gmx.de>
2023-11-22 15:58:14 +0100

    add eventTimestamp and rename eventValue

--- src/services/analytics-service/AnalyticsService.ts
+++ src/services/analytics-service/AnalyticsService.ts
@@ -10,25 +10,36 @@ export class AnalyticsService {
 
   constructor(private readonly _baseUrl: string) {}
 
-  public postEvent(analyticsEvent: IAnalyticsEvent, value?: string) {
+  public postEvent(analyticsEvent: IAnalyticsEvent, eventValue?: string) {
     if (!this.sessionId) {
       throw new Error('SessionId is undefined');
     }
 
-    if (!(analyticsEvent.label === 'session_id') && !value) {
+    if (!(analyticsEvent.label === 'session_id') && !eventValue) {
       throw new Error('Value is undefined');
     } else {
-      value = this.sessionId;
+      eventValue = this.sessionId;
     }
+
+    // Required format: %Y-%m-%d %H:%M:%S (eg: 2020-11-18 07:07:19)
+    const eventTimestamp = new Date().toISOString().replace('T', ' ').slice(0, -5);
 
     // While the backend is not ready, we will log the events to the console
     console.log('Analytics event sent:');
-    console.log(JSON.stringify({ ...analyticsEvent, value }));
+    console.log(JSON.stringify({ ...analyticsEvent, eventValue, eventTimestamp }));
 
-    // axios.post(`${this._baseUrl}/analytics`, {
-    //   ...analyticsEvent,
-    //   session_id: this.sessionId,
-    //   value,
-    // });
+    // axios.post(
+    //   `${this._baseUrl}/analytics`,
+    //   {
+    //     ...analyticsEvent,
+    //     eventValue,
+    //     eventTimestamp,
+    //   },
+    //   {
+    //     headers: {
+    //       'X-Session-Id': this.sessionId,
+    //     },
+    //   }
+    // );
   }
 }
