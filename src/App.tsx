c14a3102f32915b08544c68ea5250d931d777ba1
Svenstar74 <sven.firmbach@gmx.de>
2023-12-25 21:32:01 +0100

    remove helmet dependency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import * as Sentry from '@sentry/react';
 import { MuiThemeProvider } from '@material-ui/core';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import './common/styles/global.scss';
 import '@material/react-material-icon/dist/material-icon.css';
@@ -11,20 +10,9 @@ import router from './router/Router';
 
 const App = () => {
   return (
-    <>
-      <HelmetProvider>
-        <Helmet>
-          {/* Adding mui icons form google fonts for more icons */}
-          <link
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            rel="stylesheet"
-          />
-        </Helmet>
-        <MuiThemeProvider theme={CMTheme}>
-          <RouterProvider router={router} />
-        </MuiThemeProvider>
-      </HelmetProvider>
-    </>
+    <MuiThemeProvider theme={CMTheme}>
+      <RouterProvider router={router} />
+    </MuiThemeProvider>
   );
 };
 
