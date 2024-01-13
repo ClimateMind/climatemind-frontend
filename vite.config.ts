3b211b13b2966ebb31135120c7292d4d1e50a03c
Svenstar74 <sven.firmbach@gmx.de>
2024-01-13 10:41:06 +0100

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'build',
+  },
   resolve: {
     alias: {
       src: '/src',
