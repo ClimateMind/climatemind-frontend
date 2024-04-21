0bc72cf8fcfd05f432dbd2c6835486b93ce9400a
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-04-21 18:11:50 +0100

    [Closes #491] - Fix UserbSharedSummary component being unable to display card details (#505)
    * added alignmentScoresId via redux useAppSelector
    
    * implementing new api - ready to convert to custom hook
    
    * added query for fetch
    
    * adding package.lock
    
    * fixed endpoint issue and card content now working
    
    * added some simple styling and clean up
    
    * clean up
    
    * cleanup
    
    ---------
    
    Co-authored-by: Svenstar74 <sven.firmbach@gmx.de>

--- src/features/userB/components/UserBShareSummaryImpactCard.tsx
+++ src/features/userB/components/UserBShareSummaryImpactCard.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { CmButton, CmCard, CmTypography } from "shared/components";
-import { TSharedImpactDetails } from "types/SharedImpactDetails";
-import UserBSharedImpactDetailsModal from "./UserBSharedImpactDetailsModal";
-import { capitalizeFirstLetter } from "helpers/capitalizeFirstLetter";
-import { useApiClient } from "shared/hooks";
+import { CmButton, CmCard, CmTypography } from 'shared/components';
+import { useApiClient } from 'shared/hooks';
+import { TSharedImpactDetails } from 'types/SharedImpactDetails';
+import { capitalizeFirstLetter } from 'helpers/capitalizeFirstLetter';
+import UserBSharedImpactDetailsModal from './UserBSharedImpactDetailsModal';
 
 interface Props {
   effectId: string;
@@ -30,20 +30,16 @@ function UserBShareSummaryImpactCard({ effectId }: Props) {
   }
 
   return (
-    <CmCard style={{ padding: 20, border: '1px solid #A347FF' }}>
-      <CmTypography variant='overline' style={{ margin: 0 }}>CLIMATE EFFECT</CmTypography>
-      <CmTypography variant='h2' style={{ textAlign: 'left', margin: 0 }}>{capitalizeFirstLetter(effectDetails.effectTitle)}</CmTypography>
+    <CmCard style={{ padding: 20, border: '1px solid #A347FF', marginBottom: 10 }}>
+      <CmTypography variant="overline" style={{ margin: 0 }}>CLIMATE EFFECT</CmTypography>
 
-      <CmButton variant='text' text="Learn More" style={{ alignSelf: 'flex-start', marginTop: 20 }} onClick={() => setShowDetails(true)} />
+      <CmTypography variant="h2" style={{ textAlign: 'left', margin: 0 }}>
+        {capitalizeFirstLetter(effectDetails.effectTitle)}
+      </CmTypography>
 
-      {showDetails && (
-        <UserBSharedImpactDetailsModal
-          effectId={effectId}
-          showDetails
-          {...effectDetails}
-          onClose={() => setShowDetails(false)}
-        />
-      )}
+      <CmButton variant="text" text="Learn More" style={{ alignSelf: 'flex-start', marginTop: 20 }} onClick={() => setShowDetails(true)} />
+
+      {showDetails && <UserBSharedImpactDetailsModal effectId={effectId} showDetails {...effectDetails} onClose={() => setShowDetails(false)} />}
     </CmCard>
   );
 }
