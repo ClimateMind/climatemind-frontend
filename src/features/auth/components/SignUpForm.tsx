4861062cdf40e46eba4dd9d7e39447c135365d89
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-07-08 11:23:32 +0100

    pushing changes to frontend for auth button

--- src/features/auth/components/SignUpForm.tsx
+++ src/features/auth/components/SignUpForm.tsx
@@ -24,6 +24,8 @@ function SignUpForm({ isLoading, onCancel, onSignUp }: Props) {
   }
 
   function handleGoogleAuth() {
+    //need to set isloggedin to true so that the user is redirected to the climate feed page
+
     window.location.href = `${process.env.REACT_APP_API_URL}/register/google?quizId=${quizId}`;
 
     // console.log('googleAuth route used');
